c260cb40ce03d044171035524dc2f437cd0dd9c1
afreccia-pixel <afreccia@gmail.com>
2026-07-14 21:29:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: 'Sindico',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
